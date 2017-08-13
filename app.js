fafc7a17ce90c2777285b509f76c181e97f61c86
Steven Trefethen <steve@stevetrefethen.com>
2017-08-13 12:08:06 -0700

    Add cookie store

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 #!/usr/bin/env node --harmony
 const program = require('commander');
-const request = require('request');
 const json_colorz = require('json-colorz');
 const chalk = require('chalk');
+const FileCookieStore = require('tough-cookie-filestore');
+const fs = require("fs");
+var request = require('request');
 
 var headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
 }
 
+var cookiepath = "cookies.json";
+
+// create the json file if it does not exist
+if(!fs.existsSync(cookiepath)){
+    fs.closeSync(fs.openSync(cookiepath, 'w'));
+}
+
+// use the FileCookieStore with the request package
+var jar = request.jar(new FileCookieStore(cookiepath));
+request = request.defaults({ jar : jar });
+
 program
   .arguments('<url>')
+  .option('-m --method <method>', 'HTTP method', /^(GET|POST|PUT)$/i, 'GET')
   .option('-u, --username <username>', 'The user to authenticate as')
   .option('-p, --password <password>', 'The user\'s password')
   .action(function(url) {
@@ -20,7 +34,7 @@ program
       request(
         {
           url : url,
-          method: 'GET',
+          method: program.method,
           headers: headers,
           strictSSL: false,
           jar: true,
@@ -28,8 +42,10 @@ program
         , function(error, response, body) {
           if (error) {
             console.log(chalk.red(error));
+            process.exit(1);
           } else {
             json_colorz(JSON.parse(body));
+            process.exit(0);
           }
         }
       )
